8b9614cf4d8902df75d1e9a70788b5016dbef77a
Adelin Prelipcean <adelinprelipcean@users.noreply.github.com>
2025-07-26 19:24:34 +0300

    Day 33 - ISS Notifier

--- day 33 - API Endpoints & API Parameters - ISS Overhead Notifier/ISS Overhead Notifier/main.py
+++ day 33 - API Endpoints & API Parameters - ISS Overhead Notifier/ISS Overhead Notifier/main.py
@@ -1,18 +1,13 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
+import time
+import smtplib
 
-MY_LAT = 47.886483 # Your latitude
-MY_LONG = 25.611986 # Your longitude
+my_email = '' # Your Email
+my_password = '' # Your Password
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
-
+MY_LAT = 0 # Your latitude
+MY_LONG = 0 # Your longitude
 
 parameters = {
     "lat": MY_LAT,
@@ -20,22 +15,23 @@ parameters = {
     "formatted": 0,
 }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-print(data)
-print(sunrise)
-print(sunset)
-
-time_now = datetime.now()
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
-
+while True:
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    
+    if abs(MY_LAT - iss_latitude) <= 5 and abs(MY_LONG - iss_longitude) <= 5 and (datetime.now(timezone.utc).hour >= sunset or datetime.now(timezone.utc).hour <= sunrise):
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email, to_addrs='', msg='Subject: Watch the sky!\n\nLook at the ISS!')
+    time.sleep(3600)
 
